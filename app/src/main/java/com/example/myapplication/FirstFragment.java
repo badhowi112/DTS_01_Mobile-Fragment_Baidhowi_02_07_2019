26ecb1e44acda0e71e3373955040f89431dc9015
baidhowi112 <badhowi112@gmail.COM>
2019-07-02 22:48:56 +0700

    Done...

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -1,6 +1,5 @@
 package com.example.myapplication;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,10 +8,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+//import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class FirstFragment extends Fragment {
     View view;
     Button firstButton;
@@ -20,16 +17,15 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_first,container,false );
+
+        view = inflater.inflate(R.layout.fragment_first, container, false);
         firstButton = (Button) view.findViewById(R.id.firstButton);
         firstButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "First Fragment", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "First Fragment", Toast.LENGTH_LONG).show();
             }
         });
         return view;
     }
-
 }
--- app/src/main/java/com/example/myapplication/SecondFragment.java
+++ app/src/main/java/com/example/myapplication/SecondFragment.java
@@ -1,6 +1,5 @@
 package com.example.myapplication;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,27 +8,35 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+//import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class SecondFragment extends Fragment {
-View view;
-Button secondButton;
+    View view;
+    Button secondButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+// Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_second, container, false);
-        secondButton = (Button) view.findViewById(R.id.SeconButton);
+
+// get the reference of Button
+        secondButton = (Button) view.findViewById(R.id.secondButton);
+
+// perform setOnClickListener on second Button
         secondButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "Second Fragment", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+
+// display a message by using a Toast
+                Toast.makeText(getActivity(), "Second Fragment", Toast.LENGTH_LONG).show();
+
             }
         });
-        return view;
-    }
 
+        return view;
+
+    }
 }
